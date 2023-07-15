d8c73131427f1e9354af9cbed4211938bb6e5905
Jongwon Lee <jongwonlee12345@gmail.com>
2023-07-14 18:36:52 -0700

    profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,91 +1,64 @@
-import 'package:easy_tracker/utils/themes.dart';
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_tracker/model/user.dart';
+import 'package:easy_tracker/screens/edit_profile.dart';
+import 'package:easy_tracker/utils/user_preferences.dart';
+import 'package:easy_tracker/widgets/appbar_widget.dart';
+import 'package:easy_tracker/widgets/button_widget.dart';
+import 'package:easy_tracker/widgets/profile_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
-
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  ProfilePageState createState() => ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Profile Page",
-          style: bodyLarge,
-        )
-      )
+    const user = UserPreferences.myUser;
+
+    return ThemeSwitchingArea(
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: buildAppBar(context),
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                imagePath: user.imagePath,
+                onClicked: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => EditProfilePage()),
+                  );
+                },
+              ),
+              const SizedBox(height: 24),
+              buildName(user),
+              const SizedBox(height: 48),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.email,
+            style: const TextStyle(color: Colors.grey),
+          ),
+          Text(
+            user.phone,
+            style: const TextStyle(color: Colors.grey),
+          )
+        ],
+      );
 }
 
-
-
-// <div class="iphone-14-pro-max-profile-page-jvH" id="45:565">
-// <div class="auto-group-nysw-Mwf" id="4PWEBsPaPfUDEZzz53nysw">
-// <div class="top-nav-bar-eQy" id="51:885">
-// <div class="group-209-xRf" id="51:886">
-// <p class="time-JVX" id="51:897">08:34</p>
-// <div class="auto-group-wg1f-Pms" id="4PWEN2mK1eXVcwKnhRwg1f">
-// <div class="network-JP3" id="51:888">
-// <div class="auto-group-zzlm-RTf" id="4PWEYrnbuJWEkcDCiAZZLM">
-// <div class="rectangle-2-XWh" id="51:889"></div>
-// <div class="rectangle-3-Sdf" id="51:890"></div>
-// <div class="rectangle-4-azm" id="51:891"></div>
-// </div>
-// <div class="rectangle-5-LU9" id="51:892"></div>
-// </div>
-// <p class="tower-fmK" id="51:887">4G</p>
-// <div class="battery-normal-tty" id="51:893">
-// <div class="auto-group-bsf3-dLm" id="4PWEjgotnxUytH6ciuBSf3">
-// <div class="rectangle-8-jeh" id="51:895"></div>
-// </div>
-// <div class="rectangle-7-XaZ" id="51:896"></div>
-// </div>
-// </div>
-// </div>
-// </div>
-// <div class="main-body-ev5" id="51:927">
-// <div class="profile-bKX" id="45:580">
-// <img class="profile-frame-if3" src="/api/try-prod-us-east-2-first-cluster/projects/3qD..." id="45:582"/>
-// <div class="button-dn1" id="94:737">Edit Profile</div>
-// </div>
-// <div class="profile-info-r8y" id="45:667">
-// <div class="username-aqf" id="79:167">
-// <div class="left-XW1" id="79:168">USERNAME:</div>
-// <div class="right-oiR" id="79:171">
-// <p class="someone-NFj" id="79:172">Someone</p>
-// <div class="button-J9P" id="94:740">Edit</div>
-// </div>
-// </div>
-// <div class="email-k1P" id="79:175">
-// <div class="left-5pM" id="79:176">EMAIL:</div>
-// <div class="right-Aqo" id="79:178">
-// <p class="gmailcom-Kyb" id="79:179">*****@gmail.com</p>
-// <div class="button-Tpu" id="94:743">Edit</div>
-// </div>
-// </div>
-// <div class="phone-7eZ" id="79:182">
-// <div class="left-Fkm" id="79:183">PHONE:</div>
-// <div class="right-dWR" id="79:185">
-// <p class="item-5566-bCM" id="79:186">******5566</p>
-// <div class="button-j3f" id="94:745">Edit</div>
-// </div>
-// </div>
-// <div class="button-1G5" id="146:217">Change Password</div>
-// </div>
-// <div class="delete-profile-box-Dd3" id="79:191">
-// <div class="button-meZ" id="95:408">Log out</div>
-// <div class="button-FZj" id="94:726">Delete Profile</div>
-// </div>
-// </div>
-// </div>
-// <div class="bottom-nav-bar-jjo" id="94:432">
-// <img class="menu-5Ym" src="/api/try-prod-us-east-2-first-cluster/projects/3qD..." id="I94:432;94:352"/>
-// <img class="home-1xD" src="/api/try-prod-us-east-2-first-cluster/projects/3qD..." id="I94:432;94:357"/>
-// <img class="profile-Ljb" src="/api/try-prod-us-east-2-first-cluster/projects/3qD..." id="I94:432;94:360"/>
-// </div>
-// </div>
---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,11 @@
+import 'package:easy_tracker/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath: 'https://www.vecteezy.com/free-vector/default-profile-picture',
+    name: 'Test',
+    email: 'test@gmail.com',
+    phone: '555-555-5555',
+    isDarkMode: false,
+  );
+}
