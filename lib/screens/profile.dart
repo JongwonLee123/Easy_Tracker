fd0520ddee1c26d9f10954aefc1629ca0c7638b0
Jongwon Lee <jongwonlee12345@gmail.com>
2023-07-29 13:20:02 -0700

    profilepage

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -4,13 +4,12 @@ import 'package:flutter/material.dart';
 // Local
 import 'package:easy_tracker/utils/app_user.dart';
 import 'package:easy_tracker/utils/themes.dart';
+import 'package:easy_tracker/widgets/profile_widget.dart';
+
 
 class ProfilePage extends StatefulWidget {
   final AppUser appUser;
-  const ProfilePage({
-    Key? key,
-    required this.appUser
-  }) : super(key: key);
+  const ProfilePage({Key? key, required this.appUser}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -19,13 +18,32 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
           "Profile Page",
           style: bodyLarge,
-        )
-      )
+        ),
+        backgroundColor: fgWhite,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileWidget(
+              email: widget.appUser.email,
+              name: widget.appUser.name,
+              appUser: widget.appUser, // Pass the appUser instance to the ProfileWidget
+              onUpdateName: (newName) {
+                setState(() {
+                  widget.appUser.name = newName; // Update the name in the appUser instance
+                });
+              },
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
